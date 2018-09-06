dbd25a19d0af2d83aebcd8fdadbc391156d0026f
lei.wang <396844151@qq.com>
2018-09-06 09:32:03 +0800

    创建项目结构

---
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -0,0 +1,11 @@
+import React,{ Component } from 'react';
+
+export default class DefaultLayout extends Component{
+    render(){
+        return(
+            <div id="DefaultLayout">
+                HelloWorld
+            </div>
+        )
+    }
+}
